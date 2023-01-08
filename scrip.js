6a16afcaab9c05e84a7f270e9f88434e947def8b
phi_ironman <nguyentruongphi15032003@gmail.com>
2023-01-08 22:50:41 +0700

    code demo

--- scrip.js
+++ scrip.js
@@ -111,3 +111,23 @@ var swiper = new Swiper(".blogs-slider", {
         },
     },
 });
+
+var swiper = new Swiper(".clients-slider", {
+    loop:true, 
+    grabCursor:true,
+    spaceBetween: 20,
+    breakpoints: {
+        0: {
+          slidesPerView: 1,
+        },
+        640: {
+          slidesPerView: 2,
+        },
+        768: {
+          slidesPerView: 3,
+        },
+        1024: {
+          slidesPerView: 4,
+        },
+    },
+});
