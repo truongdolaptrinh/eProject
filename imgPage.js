6b086e64d71a913069de8e019182806fbbe73a68
phi_ironman <nguyentruongphi15032003@gmail.com>
2023-02-18 23:08:26 +0700

    updata all pages

---
+++ imgPage.js
@@ -0,0 +1,52 @@
+const cardImg = document.querySelector('.wapper_image');
+const cardFilm = document.querySelector('.card-film');
+
+eventListeners();
+
+function eventListeners(){
+    window.addEventListener('DOMContentLoaded', () => {
+        loadingJSONImage();
+        loadingJSONFilm();
+    });
+}
+
+function loadingJSONImage() {
+    fetch('images.json')
+    .then(reponse => reponse.json())
+    .then(data => {
+        let html = '';
+        data.forEach(image => {
+            html += `
+                <div class="image">
+                    <img src="${image.imageSrc}" alt="">
+                    <p>${image.name}</p>
+                </div>
+            `;
+        });
+        cardImg.innerHTML = html;
+    });
+}
+
+function loadingJSONFilm() {
+    fetch('films.json')
+    .then(reponse => reponse.json())
+    .then(data => {
+        let html = '';
+        data.forEach(film => {
+            html += `
+                <div>
+                    <h3>${film.movieName}</h3>
+                    <div class="img-film">
+                        <img src="${film.imageFilm}" alt="">
+                    </div>
+                    <p><b>Length</b>: ${film.time}</p>
+                    <p><b>Where to watch</b>: ${film.watch}</p>
+                    <p>${film.content1}</p>
+                    <p>${film.content2}</p>
+                <div>
+            `;
+        });
+        cardFilm.innerHTML = html;
+    });
+}
+
